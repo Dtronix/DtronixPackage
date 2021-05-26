1259305af0e6ff9c9dc564731a5928c3b0d6d43b
DJGosnell <DJGosnell@gmail.com>
2021-05-26 17:18:52 -0400

    Added PackageUpgradeVersion to contain all the versions of structural changes to packages.

---
+++ src/DtronixPackage/PackageUpgradeVersion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DtronixPackage
+{
+    public static class PackageUpgradeVersion
+    {
+        public static Version Unversioned { get; } = new Version();
+        public static Version V1_1_0 { get; } = new Version(1, 1, 0);
+    }
+}
